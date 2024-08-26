557ae182fecdd1c3d982349ad8e7644c33f8ac8e
maxiladner <mamoladner@gmail.com>
2024-08-25 21:54:39 -0300

    Prime commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace MI_ABM
+{
+    partial class Libreria
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.Bagregar = new System.Windows.Forms.Button();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtFono = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtgContactos = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgContactos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.cmbTipo);
+            this.groupBox1.Controls.Add(this.Bagregar);
+            this.groupBox1.Controls.Add(this.txtCorreo);
+            this.groupBox1.Controls.Add(this.txtFono);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(185, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(426, 425);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gestion contactos";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtID.Location = new System.Drawing.Point(115, 43);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(283, 36);
+            this.txtID.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 28);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "ID";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.Black;
+            this.btnCancelar.Location = new System.Drawing.Point(62, 374);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(318, 45);
+            this.btnCancelar.TabIndex = 16;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnModificar.Enabled = false;
+            this.btnModificar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificar.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.ForeColor = System.Drawing.Color.Black;
+            this.btnModificar.Location = new System.Drawing.Point(162, 309);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(117, 49);
+            this.btnModificar.TabIndex = 15;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnEliminar.Enabled = false;
+            this.btnEliminar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.Color.Black;
+            this.btnEliminar.Location = new System.Drawing.Point(296, 309);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(102, 49);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Cliente",
+            "Proveedor",
+            "Ambos"});
+            this.cmbTipo.Location = new System.Drawing.Point(115, 228);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(226, 36);
+            this.cmbTipo.TabIndex = 12;
+            this.cmbTipo.Text = "Seleccione tipo";
+            // 
+            // Bagregar
+            // 
+            this.Bagregar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.Bagregar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Bagregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bagregar.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bagregar.ForeColor = System.Drawing.Color.Black;
+            this.Bagregar.Location = new System.Drawing.Point(39, 309);
+            this.Bagregar.Name = "Bagregar";
+            this.Bagregar.Size = new System.Drawing.Size(102, 49);
+            this.Bagregar.TabIndex = 11;
+            this.Bagregar.Text = "Agregar";
+            this.Bagregar.UseVisualStyleBackColor = false;
+            this.Bagregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCorreo.Location = new System.Drawing.Point(115, 136);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(283, 36);
+            this.txtCorreo.TabIndex = 10;
+            // 
+            // txtFono
+            // 
+            this.txtFono.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtFono.Location = new System.Drawing.Point(115, 182);
+            this.txtFono.Name = "txtFono";
+            this.txtFono.Size = new System.Drawing.Size(283, 36);
+            this.txtFono.TabIndex = 9;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtNombre.Location = new System.Drawing.Point(115, 88);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(283, 36);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 28);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Fono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 28);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Correo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tipo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1044, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(11, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dtgContactos
+            // 
+            this.dtgContactos.AllowUserToResizeRows = false;
+            this.dtgContactos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgContactos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dtgContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgContactos.Location = new System.Drawing.Point(734, 122);
+            this.dtgContactos.Name = "dtgContactos";
+            this.dtgContactos.ReadOnly = true;
+            this.dtgContactos.RowHeadersWidth = 51;
+            this.dtgContactos.RowTemplate.Height = 24;
+            this.dtgContactos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgContactos.Size = new System.Drawing.Size(548, 308);
+            this.dtgContactos.TabIndex = 2;
+            this.dtgContactos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgContactos_CellClick);
+            this.dtgContactos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgContactos_CellContentClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(555, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(212, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Creado por Maximo Ladner";
+            // 
+            // Libreria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1382, 739);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtgContactos);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Libreria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Libreria";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgContactos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtFono;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Bagregar;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dtgContactos;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MI_ABM
+{
+    public partial class Libreria : Form
+    {
+        private DataGridView miDataGridView;
+        public Libreria()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Contactos contacto = new Contactos();
+            contacto.Cargar_Contactos(dtgContactos);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+      
+            string nombre = txtNombre.Text;
+            string correo = txtCorreo.Text;
+            string fono = txtFono.Text;
+            string tipo = cmbTipo.Text;
+
+            if (nombre == "" || correo == "" || fono == "" || tipo == "Seleccione tipo")
+            {
+                MessageBox.Show("Debe completar todos los campos");
+            }
+            else
+            {
+                Contactos nuevoContacto = new Contactos(0,nombre, correo, fono, tipo);
+                int fila = nuevoContacto.AgregarContacto();
+                if(fila == 1)
+                {
+                    MessageBox.Show("El registro se agrego correctamente",
+                        "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtID.Text = "";
+                    txtNombre.Text = "";
+                    txtCorreo.Text = "";
+                    txtFono.Text = "";
+                    cmbTipo.Text = "Seleccione tipo";
+                    ListarContactos();
+                }
+                else
+                {
+                    MessageBox.Show("Ocurrio un error al agregar el registro",
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtgContactos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+             
+            ListarContactos();
+        }
+        public void ListarContactos()
+        {
+            Contactos contactos = new Contactos();
+            contactos.Cargar_Contactos(dtgContactos);
+        }
+        private void dtgContactos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int indice = e.RowIndex;
+
+            if (indice == -1 || dtgContactos.SelectedCells[1].Value.ToString() == "")
+            {
+                ReseteaFomulario();
+            }
+            else
+            {
+                txtID.Text = dtgContactos.SelectedCells[0].Value.ToString();
+                txtNombre.Text = dtgContactos.SelectedCells[1].Value.ToString();
+                txtCorreo.Text = dtgContactos.SelectedCells[2].Value.ToString();
+                txtFono.Text = dtgContactos.SelectedCells[3].Value.ToString();
+                cmbTipo.Text = dtgContactos.SelectedCells[4].Value.ToString();
+
+                Bagregar.Enabled = false;
+                btnEliminar.Enabled = true;
+                btnModificar.Enabled = true ;
+            }
+        }
+        public void ReseteaFomulario()
+        {
+            txtNombre.Clear();
+            txtCorreo.Clear();
+            txtFono.Clear();
+            txtID.Clear();
+            cmbTipo.Text = "Seleccione tipo";
+            Bagregar.Enabled = true;
+            btnEliminar.Enabled = false;
+            btnModificar.Enabled = false;
+            txtNombre.Focus();
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            ReseteaFomulario();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtID.Text);
+
+            DialogResult confirmar = MessageBox.Show("¿Desea eliminar?", "Mensaj", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if(confirmar == DialogResult.OK)
+            {
+                Contactos contacto = new Contactos(id);
+                int fila = contacto.EliminarContacto();
+
+                if (fila == 1)
+                {
+                    MessageBox.Show("Se elimino el contacto exitosamente");
+                    ReseteaFomulario();
+                    Contactos contactos = new Contactos();
+                    contactos.Cargar_Contactos(dtgContactos);
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo elimina el contacto");
+                }
+            }
+            else
+            {
+                ReseteaFomulario();
+            }
+                
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtID.Text);
+            string nombre = txtNombre.Text;
+            string correo = txtCorreo.Text;
+            string fono = txtFono.Text;
+            string tipo = cmbTipo.Text;
+            DialogResult confimar = MessageBox.Show("Desea realizar los cambios?","Mensaje",MessageBoxButtons.OKCancel,MessageBoxIcon.Question);    
+
+
+            if(confimar == DialogResult.OK)
+            {
+                Contactos contacto = new Contactos(id,nombre,correo,fono,tipo);
+                int fila = contacto.EditarContacto();
+                if(fila == 1)
+                {
+                    MessageBox.Show("Se actualizo el registro correctamente");
+                    ReseteaFomulario();
+                    ListarContactos();
+
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo actualizar el contacto"); 
+                }
+            }
+            else
+            {
+                ReseteaFomulario();
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
